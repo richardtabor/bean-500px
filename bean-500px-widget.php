6373ab3e88f3fe4c6d94d40abed9dac051a9a11c
kamilwaheed <kamilwaheed@icloud.com>
2014-06-08 16:13:08 +0500

    Initial Commit

---
+++ bean-500px-widget.php
@@ -0,0 +1,473 @@
+<?php 
+/*--------------------------------------------------------------------
+
+    Widget Name: Bean 500px Widget
+    Widget URI: http://www.themebeans.com/?ref=plugin_bean_500px
+    Description:  A widget that displays your most recent 500px photos, your 500px feed (photos of friends your following) or your favorited posts on 500px
+    Author: Kamil Waheed / ThemeBeans
+    Author URI: http://www.themebeans.com/?ref=plugin_bean_500px
+    Version: 1.0
+
+/*--------------------------------------------------------------------*/
+
+
+// WIDGET CLASS
+class widget_bean_500px extends WP_Widget {
+
+    private $API_CONSUMER_KEY = "XDtqmkDVnElsZvENizrfyWmJ8W2FSVJBEkJrPjiD";
+    private $API_URI_BASE = "https://api.500px.com/v1/";
+    private $API_PHOTOS_ENDPOINT = "photos";
+
+    private $db_option_key = "bean500px_settings";
+
+    public function __construct() {
+        parent::__construct(
+            'bean_500px', // BASE ID
+            'Bean 500px', // NAME
+            array( 'description' => __( 'Displays photos from 500px.', 'bean' ), )
+        );
+
+        if ( is_active_widget(false, false, $this->id_base) )
+            add_action( 'wp_head', array(&$this, 'load_widget_style') );
+    }
+
+
+    /** 
+     * Widget style is loaded based on the currently activated theme
+     * If an associated style exists under /themes, it is loaded
+     * /themes/_default/500px.css is loaded otherwise
+     * 
+     * hooked to wp_head
+     *
+     * @return void
+     */
+    public function load_widget_style() {
+        $current_theme = wp_get_theme();
+
+        $theme_css_path = DIRNAME(__FILE__) . '/themes/' . $current_theme . '/500px.css';
+        $theme_css_url = plugin_dir_url(__FILE__) . 'themes/' . $current_theme . '/500px.css';
+        $default_css_url = plugin_dir_url(__FILE__) . 'themes/_default/500px.css';
+
+        // fix spaces that may exist in the theme name
+        $theme_css_url = str_replace(' ', '%20', $theme_css_url);
+
+        if (file_exists($theme_css_path))
+            wp_enqueue_style( 'bean-500px-style', $theme_css_url, false, '1.0', 'all' );
+        else
+            wp_enqueue_style( 'bean-500px-style', $default_css_url, false, '1.0', 'all' );
+    }
+
+    /**
+     * Render the front-end widget output
+     * 
+     * @return void
+     */
+    public function widget($args, $instance) {
+        extract($args);
+        if(!empty($instance['title'])){ $title = apply_filters( 'widget_title', $instance['title'] ); }
+
+        echo $before_widget;
+        if ( ! empty( $title ) ){ echo $before_title . $title . $after_title; }
+
+        $desc = $instance['desc'];
+        if($desc != '') : ?><p><?php echo $desc; ?></p><?php endif;
+
+        $photos = $this->get500px_photos( $instance['username'], $instance['show'], $instance['cachetime'] );
+
+        if ( !$photos ) {
+            $error = new stdClass();
+            $error->status = "001";
+            $error->error = "Failed to retrieve response from 500px. Check if the host has access to internet.";
+
+            $this->render_error( $error );
+        }
+        else if ( isset( $photos->error ) ) {
+            $this->render_error($photos);
+        } else {
+            $photos = $photos->photos;
+
+            echo "<div class='bean500px-image-wrapper'>";  
+
+            if (count($photos) > 0) {          
+                foreach($photos as $photo) {
+                    $this->render_photo( $photo );
+                }
+            } else {
+                echo "<p>";
+                echo "500px returned 0 photos for the current settings.";
+                echo "</p>";
+            }
+
+            echo "</div>";            
+        }
+
+        echo $after_widget;
+    }
+
+
+    /**
+     * Based a $photo object, render a front-end image markup
+     *
+     * @param object $photo An object as returned from the 500px API
+     *
+     * @return void
+     */
+    private function render_photo($photo) {
+        $image = $photo->images;
+        $image = $image[0];
+
+        $image_src = $image->url;
+        $image_name = $photo->name;
+        $image_link = "http://500px.com" . $photo->url;
+
+        echo "<div class='bean500px_badge_image fadein'>";
+        echo "<a href='$image_link' target='_blank'>";
+        echo "<img src='$image_src' alt='$image_name'>";
+        echo "</a>";
+        echo "</div>";
+    }
+
+    /**
+     * Render the error markup given the error object $error
+     *
+     * @param object $error An error object as returned from the 500px API
+     *
+     * @return void
+     */
+    private function render_error($error) {
+        echo "<p>";
+        echo "<strong>";
+        echo __("500px API error!", "bean");
+        echo "</strong>";
+        echo  "</p>";
+
+        echo "<p>";
+        echo $error->error;
+        echo "(" . $error->status . ")";
+        echo "</p>";
+    }
+
+
+    /**
+     * The callback function to returned the sanitized values of the widget settings
+     *
+     * @param array $new_instance An array with key/value pairs of new settings
+     * @param array $old_instance An array with key/value pairs of old (current) settings
+     *
+     * @return array Sanitized array with key/value pairs of settings to save
+     */
+    public function update($new_instance, $old_instance) {
+        $instance = array();
+
+        $settings = $this->get_widget_settings();
+
+        foreach($settings as $key => $value) {
+            if ( ! isset( $new_instance[$key] ) ) continue;
+
+            switch($value['type']) {
+                case 'textarea':
+                    $instance[$key] = stripslashes( $new_instance[$key] );
+
+                    break;
+
+                default:
+                    $instance[$key] = strip_tags( $new_instance[$key] );
+
+                    break;
+            }
+        }
+
+        $this->force_invalidate_raw_response_cache();
+
+        return $instance;
+    }
+
+    /**
+     * Returns a pre-defined array of all the settings to be saved and rendered by the widget
+     *
+     * @return array The settings array
+     */
+    private function get_widget_settings() {
+        return array(
+            'title'        => array(
+                                'title'     => __("Title", "bean"),
+                                'default'   => __("Bean 500px Plugin", "bean"),
+                                'type'      => "full-text"
+                           ),
+            'desc'         => array(
+                                'title'     => "",
+                                'default'   => "",
+                                'type'      => "textarea",
+                                'args'      => array(
+                                                    'top'   => '-8px',
+                                                    'rows'  => "5",
+                                                    'cols'  => "15"
+                                               )
+                           ),
+            'username'     => array(
+                                'title'     => __("Username", "bean"),
+                                'default'   => "",
+                                'type'      => "full-text"
+                           ),
+            'show'         => array(
+                                'title'     => __("Show", "bean"),
+                                'default'   => "recent",
+                                'type'      => "select",
+                                'args'      => array(
+                                                    'options'   => array(
+                                                                        'user'              => __("Recently Uploaded", "bean"),
+                                                                        'user_friends'      => __("Following", "bean"),
+                                                                        'user_favorites'    => __("Favorites", "bean")
+                                                                   )
+                                               )
+                           ),
+            'cachetime'    => array(
+                                'title'     => __("Cache Time", "bean"),
+                                'default'   => "5",
+                                'type'      => "small-text",
+                                'args'      => array(
+                                                    'suffix'    => 'hours'
+                                               )
+                           ),
+        );
+    }
+
+
+    /**
+     * Render the widget settings panel (under WP Admin -> Appearance -> Widgets)
+     *
+     * @return void
+     */
+    public function form($instance) {
+        $settings = $this->get_widget_settings();
+
+        // construct the defaults array
+        $defaults = array();
+        foreach($settings as $key => $value) {
+            $defaults[$key] = $value['default'];
+        }
+
+        $instance = wp_parse_args( (array) $instance, $defaults );
+
+
+        foreach($settings as $key => $value) {
+            $this->generateFormField( $value['title'], 
+                                      $key,
+                                      $value['type'], 
+                                      $instance[ $key ], 
+                                      isset( $value['suffix'] ) ? $value['suffix'] : '',
+                                      isset( $value['args'] ) ? $value['args'] : ''
+                                    );
+        }
+
+    }
+
+    /**
+     * Render a form field based on the arguments
+     *
+     * @param string $title The text of the <label> for the field
+     * @param string $name The "name" attribute value to use in the markup
+     * @param string $type The type of the form field to render. Supported values are: textarea, select, small-text, and full-text
+     * @param string $value The value to populate in the form field
+     * @param string $suffix Some extra markup(or plain text) to render right after the field
+     * @param array $args Extra arguments depending on the $type of field
+     *
+     * @return void
+     */
+    private function generateFormField( $title, $name, $type, $value = "", $suffix = "", $args = array() ) {
+        $top = isset( $args['top'] ) ? $args['top'] : NULL;
+        $style = $top ? "style='margin-top: $top;'" : "";
+
+        $id = $this->get_field_id($name);
+        $name = $this->get_field_name($name);
+
+        echo "<p $style>";
+
+        if ( !empty($title) ) {
+            echo "<label for='$id'>$title&nbsp;</label>";
+        }
+
+        switch( $type ) {
+            case 'textarea':
+                $rows = isset($args['rows']) ? $args['rows'] : '';
+                $cols = isset($args['cols']) ? $args['cols'] : '';
+
+                echo "<textarea name='$name' id='$id' class='widefat' rows='$rows' cols='$cols'>$value</textarea>";
+                break;
+
+            case 'select':
+                echo "<select name='$name' class='widefat' id='$id'>";
+
+                $options = isset( $args['options'] ) ? $args['options'] : array();
+
+                foreach($options as $key => $name) {
+                    $selected = $key == $value ? "selected='selected'" : '';
+                    echo "<option value='$key' $selected >$name</option>";
+                }
+
+                echo "</select>";
+                break;
+
+            case 'small-text':
+                echo "<input type='text' name='$name' id='$id' value='$value' class='small-text'>";
+                break;
+
+            default:
+                echo "<input type='text' name='$name' id='$id' value='$value' class='widefat'>";
+                break;
+        }
+
+        if ( isset($args['suffix']) ) {
+            echo ' ' . $args['suffix'];
+        }
+
+        echo "</p>";
+    }
+
+
+    /*--------------------------------------------------------------------*/
+    /*  500px API COMMUNICATION
+    /*--------------------------------------------------------------------*/
+
+
+    /**
+     * Return the cached or new JSON object from the 500px API based on the arguments
+     *
+     * @param string $username The 'username' value to pass to 500px API
+     * @param string $feature The 'feature' value to pass to 500px API
+     * @param int $cache_duration Number of seconds the cache lasts
+     *
+     * @return object|false A JSON object as received from the 500px API; false otherwise
+     *
+     */
+    private function get500px_photos( $username, $feature, $cache_duration = 0 ) {
+        $cache = $this->get_raw_response_cache($cache_duration);
+
+        if ($cache) {
+            $raw_response = $cache;
+        } else {
+            $raw_response = $this->get500px_raw_response( $username, $feature );
+
+            $this->set_raw_response_cache( $raw_response );
+        }
+
+        if (!empty($raw_response)) {
+            $response = json_decode( $raw_response );
+            return $response;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Query the 500px API and return the raw response based on the arguments
+     *
+     * @param string $username The 'username' value to pass to 500px API
+     * @param string $feature The 'feature' value to pass to 500px API
+     *
+     * @return string The raw response string from the 500px API
+     */
+
+    private function get500px_raw_response( $username, $feature ) {
+        $endpoint_URI = $this->API_URI_BASE . $this->API_PHOTOS_ENDPOINT;
+        $url_vars = array(
+                        "consumer_key"  => $this->API_CONSUMER_KEY,
+                        "username"      => $username,
+                        "feature"       => $feature,
+                        "image_size"    => 2,
+                        "rpp"           => 8
+                    );
+
+        $endpoint_URI .= '?' . http_build_query( $url_vars );
+
+        $curl = curl_init();
+        $curl_options = array(
+            CURLOPT_URL             => $endpoint_URI,
+            CURLOPT_HEADER          => false,
+            CURLOPT_RETURNTRANSFER  => true
+        );
+
+        curl_setopt_array( $curl, $curl_options );
+        return curl_exec( $curl );
+    }
+
+
+    /*--------------------------------------------------------------------*/
+    /*  OPTIONS API INTERFACE
+    /*--------------------------------------------------------------------*/
+
+    /**
+     * Return the 500px raw response cache if saved and valid
+     *
+     * @return string|false Cache if valid; false otherwise
+     */
+    private function get_raw_response_cache( $cache_duration = 0 ) {
+        $plugin_settings = get_option( $this->db_option_key );
+
+        if ( $plugin_settings ) {
+            if ( isset( $plugin_settings['photos_cache'] ) &&
+                 isset( $plugin_settings['cache_time'] ) &&
+                 $plugin_settings['cache_time'] != 0 ) {
+
+                $current_time = time();
+                $cache_time = $plugin_settings['cache_time'];
+                $difference = $current_time - $cache_time;
+                $cache_duration = intval( $cache_duration ) * 3600;
+
+                if ($difference < $cache_duration) {
+                    return $plugin_settings['photos_cache'];
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Save the 500px raw response in cache
+     * 
+     * @param string $cache The raw cache string to save
+     *
+     * @return void
+     */
+    private function set_raw_response_cache($cache) {
+        $plugin_settings = array();
+        $saved_plugin_settings = get_option( $this->db_option_key );
+
+        if ($saved_plugin_settings) {
+            $plugin_settings = $saved_plugin_settings;
+        }
+
+        $plugin_settings['photos_cache'] = $cache;
+        $plugin_settings['cache_time'] = time();
+
+        update_option( $this->db_option_key, $plugin_settings );
+    }
+
+    /**
+     * Invalidate the 500px raw response cache that maybe saved
+     *
+     * @return void
+     */
+    private function force_invalidate_raw_response_cache() {
+        $plugin_settings = array();
+        $saved_plugin_settings = get_option( $this->db_option_key );
+
+        if ($saved_plugin_settings) {
+            $plugin_settings = $saved_plugin_settings;
+        }
+
+        $plugin_settings['cache_time'] = 0;
+
+        update_option( $this->db_option_key, $plugin_settings );
+    }
+}
+
+
+// REGISTER WIDGET
+function register_bean500px_widget(){
+    register_widget('widget_bean_500px');
+}
+add_action('init', 'register_bean500px_widget', 1);
+
+?>
---
+++ bean-500px.php
@@ -0,0 +1,137 @@
+<?php 
+/*
+Plugin Name: Bean 500px
+Plugin URI: http://themebeans.com/plugin/bean-500px-plugin/?ref=plugin_bean_500px
+Description: Shows the photos from 500px that you or your friends have uploaded, or the ones that you've favorited - right in any of the widget areas.
+Version: 1.0
+Author: Kamil Waheed / ThemeBeans
+Author URI: http://www.themebeans.com/?ref=plugin_bean_500px
+*/
+
+
+// DON'T CALL ANYTHING
+if ( !function_exists( 'add_action' ) ) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+define('BEAN_500PX_PATH', plugin_dir_url( __FILE__ ));
+
+
+/*===================================================================*/
+/*
+/* PLUGIN FEATURES SETUP
+/*
+/*===================================================================*/
+$bean_plugin_features[ plugin_basename( __FILE__ ) ] = array(
+        "updates"  => false // Whether to utilize plugin updates feature or not
+    );
+
+
+if ( ! function_exists( 'bean_plugin_supports' ) ) 
+{
+    function bean_plugin_supports( $plugin_basename, $feature ) 
+    {
+        global $bean_plugin_features;
+
+        $setup = $bean_plugin_features;
+
+        if( isset( $setup[$plugin_basename][$feature] ) && $setup[$plugin_basename][$feature] )
+            return true;
+        else
+            return false;
+    }
+}
+
+
+/*===================================================================*/
+/*
+/* PLUGIN UPDATER FUNCTIONALITY
+/*
+/*===================================================================*/
+define( 'EDD_BEAN500PX_TB_URL', 'http://themebeans.com' );
+define( 'EDD_BEAN500PX_NAME', 'Bean 500px' );
+
+if ( bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) ) : // check to see if updates are allowed; only import if so
+
+//LOAD UPDATER CLASS
+if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) 
+{
+    include( dirname( __FILE__ ) . '/updates/EDD_SL_Plugin_Updater.php' );
+}
+//INCLUDE UPDATER SETUP
+include( dirname( __FILE__ ) . '/updates/EDD_SL_Activation.php' );
+
+
+endif; // END if ( bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) )
+
+
+/*===================================================================*/
+/* UPDATER SETUP
+/*===================================================================*/
+function bean500px_license_setup() 
+{
+    add_option( 'edd_bean500px_activate_license', 'BEAN500PX' );
+    add_option( 'edd_bean500px_license_status' );
+}
+add_action( 'init', 'bean500px_license_setup' );
+
+function edd_bean500px_plugin_updater() 
+{
+    // check to see if updates are allowed; don't do anything if not
+    if ( ! bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) ) return;
+
+    //RETRIEVE LICENSE KEY
+    $license_key = trim( get_option( 'edd_bean500px_activate_license' ) );
+
+    $edd_updater = new EDD_SL_Plugin_Updater( EDD_BEAN500PX_TB_URL, __FILE__, array( 
+            'version' => '1.0',
+            'license' => $license_key,
+            'item_name' => EDD_BEAN500PX_NAME,
+            'author'    => 'ThemeBeans'
+        )
+    );
+}
+add_action( 'admin_init', 'edd_bean500px_plugin_updater' );
+
+
+/*===================================================================*/
+/* DEACTIVATION/UNINSTALLATION HOOK - REMOVE OPTION
+/*===================================================================*/
+function bean500px_deactivate() 
+{
+    if ( ! current_user_can( 'activate_plugins' ) )
+        return;
+
+    $plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+    check_admin_referer( "deactivate-plugin_{$plugin}" );
+    
+    bean500px_delete_options();
+}
+function bean500px_uinstall() {
+    if ( ! current_user_can( 'activate_plugins' ) )
+        return;
+    check_admin_referer( 'bulk-plugins' );
+
+    // Important: Check if the file is the one
+    // that was registered during the uninstall hook.
+    if ( __FILE__ != WP_UNINSTALL_PLUGIN )
+        return;
+
+    bean500px_delete_options();
+}
+function bean500px_delete_options() {
+    delete_option( 'edd_bean500px_activate_license' );
+    delete_option( 'edd_bean500px_license_status' );
+
+    delete_option( 'bean500px_settings' );
+}
+
+register_deactivation_hook( __FILE__, 'bean500px_deactivate' );
+register_uninstall_hook( __FILE__, 'bean500px_uinstall' );
+
+
+// INCLUDE WIDGET
+require_once('bean-500px-widget.php');
+
+?>
